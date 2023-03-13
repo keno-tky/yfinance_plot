8d97f9d07ad1c9edb0523d305b209b21aebc3434
Keino Takuya <airwink101@gmail.com>
2023-03-13 23:20:58 +0900

    1st commit

--- yfinance_app.py
+++ yfinance_app.py
@@ -35,49 +35,50 @@ def get_data(days, tickers):
     return df
 
 # try: 
-    st.sidebar.write("""
-    ## 株価の範囲指定
-    """)
-    ymin, ymax = st.sidebar.slider(
-        '範囲を指定してください。',
-        0.0, 3500.0, (0.0, 3500.0)
-    )
+st.sidebar.write("""
+## 株価の範囲指定
+""")
+ymin, ymax = st.sidebar.slider(
+    '範囲を指定してください。',
+    0.0, 3500.0, (0.0, 3500.0)
+)
 
-    tickers = {
-        'apple': 'AAPL',
-        'facebook': 'FB',
-        'google': 'GOOGL',
-        'microsoft': 'MSFT',
-        'netflix': 'NFLX',
-        'amazon': 'AMZN'
-    }
-    df = get_data(days, tickers)
-    companies = st.multiselect(
-        '会社名を選択してください。',
-        list(df.index),
-        ['google', 'amazon', 'facebook', 'apple']
-    )
+tickers = {
+    'apple': 'AAPL',
+    'facebook': 'FB',
+    'google': 'GOOGL',
+    'microsoft': 'MSFT',
+    'netflix': 'NFLX',
+    'amazon': 'AMZN'
+}
+df = get_data(days, tickers)
+companies = st.multiselect(
+    '会社名を選択してください。',
+    list(df.index),
+    ['google', 'amazon', 'facebook', 'apple']
+)
 
-    if not companies:
-        st.error('少なくとも一社は選んでください。')
-    else:
-        data = df.loc[companies]
-        st.write("### 株価 (USD)", data.sort_index())
-        data = data.T.reset_index()
-        data = pd.melt(data, id_vars=['Date']).rename(
-            columns={'value': 'Stock Prices(USD)'}
+if not companies:
+    st.error('少なくとも一社は選んでください。')
+else:
+    data = df.loc[companies]
+    st.write("### 株価 (USD)", data.sort_index())
+    data = data.T.reset_index()
+    data = pd.melt(data, id_vars=['Date']).rename(
+        columns={'value': 'Stock Prices(USD)'}
+    )
+    chart = (
+        alt.Chart(data)
+        .mark_line(opacity=0.8, clip=True)
+        .encode(
+            x="Date:T",
+            y=alt.Y("Stock Prices(USD):Q", stack=None, scale=alt.Scale(domain=[ymin, ymax])),
+            color='Name:N'
         )
-        chart = (
-            alt.Chart(data)
-            .mark_line(opacity=0.8, clip=True)
-            .encode(
-                x="Date:T",
-                y=alt.Y("Stock Prices(USD):Q", stack=None, scale=alt.Scale(domain=[ymin, ymax])),
-                color='Name:N'
-            )
-        )
-        st.altair_chart(chart, use_container_width=True)
+    )
+    st.altair_chart(chart, use_container_width=True)
 # except:
-    st.error(
-        "おっと！なにかエラーが起きているようです。"
-    )
+    # st.error(
+    #     "おっと！なにかエラーが起きているようです。"
+    # )
+
