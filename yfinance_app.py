3997c1b6274e2a1854846a662b2a54082a6995e5
Keino Takuya <airwink101@gmail.com>
2023-03-13 23:49:29 +0900

    1st commit

--- yfinance_app.py
+++ yfinance_app.py
@@ -1,3 +1,4 @@
+# %%
 import pandas as pd
 import yfinance as yf
 import altair as alt
@@ -20,13 +21,13 @@ st.write(f"""
 ### 過去 **{days}日間** のGAFA株価
 """)
 
-@st.cache
+@st.cache_data
 def get_data(days, tickers):
     df = pd.DataFrame()
     for company in tickers.keys():
         tkr = yf.Ticker(tickers[company])
         hist = tkr.history(period=f'{days}d')
-        hist.index = hist.index.strftime('%d %B %Y')
+        hist.index = hist.index.strftime("%d %B %Y")
         hist = hist[['Close']]
         hist.columns = [company]
         hist = hist.T
@@ -34,51 +35,51 @@ def get_data(days, tickers):
         df = pd.concat([df, hist])
     return df
 
-# try: 
-st.sidebar.write("""
-## 株価の範囲指定
-""")
-ymin, ymax = st.sidebar.slider(
-    '範囲を指定してください。',
-    0.0, 3500.0, (0.0, 3500.0)
-)
-
-tickers = {
-    'apple': 'AAPL',
-    'facebook': 'FB',
-    'google': 'GOOGL',
-    'microsoft': 'MSFT',
-    'netflix': 'NFLX',
-    'amazon': 'AMZN'
-}
-df = get_data(days, tickers)
-companies = st.multiselect(
-    '会社名を選択してください。',
-    list(df.index),
-    ['google', 'amazon', 'facebook', 'apple']
-)
-
-if not companies:
-    st.error('少なくとも一社は選んでください。')
-else:
-    data = df.loc[companies]
-    st.write("### 株価 (USD)", data.sort_index())
-    data = data.T.reset_index()
-    data = pd.melt(data, id_vars=['Date']).rename(
-        columns={'value': 'Stock Prices(USD)'}
+try: 
+    st.sidebar.write("""
+    ## 株価の範囲指定
+    """)
+    ymin, ymax = st.sidebar.slider(
+        '範囲を指定してください。',
+        0.0, 400.0, (0.0, 400.0)
     )
-    chart = (
-        alt.Chart(data)
-        .mark_line(opacity=0.8, clip=True)
-        .encode(
-            x="Date:T",
-            y=alt.Y("Stock Prices(USD):Q", stack=None, scale=alt.Scale(domain=[ymin, ymax])),
-            color='Name:N'
+
+    tickers = {
+        'apple': 'AAPL',
+        'meta': 'META',
+        'google': 'GOOGL',
+        'microsoft': 'MSFT',
+        'netflix': 'NFLX',
+        'amazon': 'AMZN'
+    }
+    df = get_data(days, tickers)
+    companies = st.multiselect(
+        '会社名を選択してください。',
+        list(df.index),
+        ['google', 'amazon', 'meta', 'apple']
+    )
+
+    if not companies:
+        st.error('少なくとも一社は選んでください。')
+    else:
+        data = df.loc[companies]
+        st.write("### 株価 (USD)", data.sort_index())
+        data = data.T.reset_index()
+        data = pd.melt(data, id_vars=['Date']).rename(
+            columns={'value': 'Stock Prices(USD)'}
         )
+        chart = (
+            alt.Chart(data)
+            .mark_line(opacity=0.8, clip=True)
+            .encode(
+                x="Date:T",
+                y=alt.Y("Stock Prices(USD):Q", stack=None, scale=alt.Scale(domain=[ymin, ymax])),
+                color='Name:N'
+            )
+        )
+        st.altair_chart(chart, use_container_width=True)
+except:
+    st.error(
+        "おっと！なにかエラーが起きているようです。"
     )
-    st.altair_chart(chart, use_container_width=True)
-# except:
-    # st.error(
-    #     "おっと！なにかエラーが起きているようです。"
-    # )
 
